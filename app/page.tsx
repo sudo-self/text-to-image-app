5e06699b4f8e07a38b1f7138a487729bfae16342
Sudo-Self <sudo-self@proton.me>
2025-04-26 00:56:42 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -243,7 +243,7 @@ wget -O generated-image.png "${API_URL}?prompt=${encodeURIComponent(prompt)}"`;
 
   return (
     <div className="container mx-auto py-8 px-4">
-      <div className="container mx-auto py-8 px-4">
+      <div className="container mx-auto py-8 px-4 bg-gray-100">
         <div className="flex justify-center mb-2">
           <img src="/texttoimage.svg" alt="text2image" className="h-52 block" />
         </div>
